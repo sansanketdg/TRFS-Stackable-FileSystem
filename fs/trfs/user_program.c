0e8508e9e4e2b4fbecf936fe3bfd952094591fb4
root <root@vl96.cs.sunysb.edu>
2016-11-02 14:32:50 -0400

    adding user_prog--memcpy

--- fs/trfs/user_program.c
+++ fs/trfs/user_program.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+struct trfs_record
+{
+int record_id;
+};
 
 void main(){
 	FILE *fileptr;
 	char *buffer;
 	long filelen;
-
-	fileptr = fopen("/usr/src/logfile.txt", "rb");  // Open the file in binary mode
+	struct trfs_record *samplerecord;
+	fileptr = fopen("/usr/src/logfile1.txt", "rb");  // Open the file in binary mode
 	fseek(fileptr, 0, SEEK_END);          // Jump to the end of the file
 	filelen = ftell(fileptr);             // Get the current byte offset in the file
 	rewind(fileptr);                      // Jump back to the beginning of the file
-
+	samplerecord=(struct trfs_record*)malloc(sizeof(struct trfs_record));
 	buffer = (char *)malloc((filelen+1)*sizeof(char)); // Enough memory for file + \0
 	fread(buffer, filelen, 1, fileptr); // Read in the entire file
 	printf("%s\n", buffer);
 	
 	int record_id = -1;
 	int buffer_offset = 0;
+	memcpy((void *)&samplerecord->record_id,(void *)(buffer + buffer_offset),  sizeof(int));
+
 	
-	memcpy((void *)&record_id, (void *)&buffer, sizeof(int));
-	printf("%d\n", i);
+	
+	printf("Record_id is %d\n", samplerecord->record_id);
 	
 	fclose(fileptr); // Close the file
-
+	free(samplerecord);
+	free(buffer);
 }
