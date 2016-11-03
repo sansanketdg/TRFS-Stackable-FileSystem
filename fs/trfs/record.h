9cbe5b2a1ec26d3242722cf28160421973149d26
root <root@vl217.cs.stonybrook.edu>
2016-11-03 00:19:54 -0400

    latest code

---
+++ fs/trfs/record.h
@@ -0,0 +1,15 @@
+#define MKDIR 2
+#define OPEN 1
+
+/* trfs record structure used to store to store in file*/
+struct trfs_record {
+	int record_id;
+	unsigned short record_size;
+	unsigned char record_type;
+	int open_flags;
+	int permission_mode;
+	short pathname_size;
+	char *pathname;
+	int return_value;
+	unsigned char mybitmap;
+};
