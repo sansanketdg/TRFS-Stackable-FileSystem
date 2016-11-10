6328f57dd90f8ae0dfedd50ae59c49270ca37fa8
root <root@vl217.cs.sunysb.edu>
2016-11-09 23:12:59 -0500

    final push

--- fs/trfs/install_module.sh
+++ fs/trfs/install_module.sh
@@ -2,8 +2,6 @@
 set -x
 # WARNING: this script doesn't check for errors, so you have to enhance it in case any of the commands
 # below fail.
-dmesg --cl
-rm -f /usr/src/logfile.txt
 lsmod
 rmmod trfs
 insmod trfs.ko
